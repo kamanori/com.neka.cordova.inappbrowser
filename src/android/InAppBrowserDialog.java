512688887d175c22325fa6069facb0095a21896d
kamanori <rdamico@neka.com>
2014-12-09 11:53:09 -0500

    Fixing Package References

--- src/android/InAppBrowserDialog.java
+++ src/android/InAppBrowserDialog.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package org.apache.cordova.inappbrowser;
+package com.neka.cordova.inappbrowser;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
